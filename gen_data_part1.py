1f258d828fb375667d6e5aaa0932c8ff8ab41ba3
vader <pradyuishere@gmail.com>
2019-02-14 11:50:04 -0800

    data generating code written

---
+++ gen_data_part1.py
@@ -0,0 +1,21 @@
+import keras
+import tf
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+###############################################################################
+##Defining the necessities
+#Defining (n, k)
+num_channels = 2
+num_messages = 4
+num_bits = 2
+###############################################################################
+##Generating the training data, one hot vectors of width num_messages
+num_samples = 10000
+training_data = np.zeros([num_samples, num_messages])
+
+for iter in range(10000):
+    training_data[iter, np.randint(4)] = 1
+
+print(training_data)
