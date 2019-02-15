3d800df336f1946b8b9f14ba8af856352061d834
vader <pradyuishere@gmail.com>
2019-02-15 02:19:54 -0800

    all files to be checked

---
+++ auto_enc_part1.py
@@ -0,0 +1,14 @@
+import keras
+import tf
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+###############################################################################
+##Defining the necessities
+#Defining (n, k)
+num_channels = 2
+message_bits = 2
+###############################################################################
+##Generating the training data
+num_samples = 10000
