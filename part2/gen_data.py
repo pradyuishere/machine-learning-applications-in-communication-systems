f8cb76d7782363844ceb74da5565c47b292e6ea9
vader <pradyuishere@gmail.com>
2019-02-19 04:40:42 -0800

    generated data

--- part2/gen_data.py
+++ part2/gen_data.py
@@ -4,32 +4,51 @@ import numpy as np
 ##Defining the necessities
 #Defining (n, k)
 num_channels = 2
-num_messages = 16
+num_messages = 4
 num_bits = 2
 ###############################################################################
 ##Generating the training data, one hot vectors of width num_messages
 num_samples = 10000
 training_data = np.zeros([num_samples, num_messages])
 
-file = open("training_data.npy", 'w')
-# file = open("training_data-b.npy", 'w')
+file = open("training_data-a.npy", 'w')
+file1 = open("training_data-b.npy", 'w')
 
 for iter in range(num_samples):
 
     training_data[iter, np.random.randint(num_messages)] = 1
 np.save(file, training_data)
 print(training_data)
+
+training_data = np.zeros([num_samples, num_messages])
+
+for iter in range(num_samples):
+    training_data[iter, np.random.randint(num_messages)] = 1
+
+np.save(file1, training_data)
+print(training_data)
 file.close()
+file1.close()
 ###############################################################################
 ##Generating the test data, one hot vectors of width num_messages
 num_samples = 5000
 test_data = np.zeros([num_samples, num_messages])
 
-file = open("test_data.npy", 'w')
+file = open("test_data-a.npy", 'w')
+file1 = open("test_data-b.npy", 'w')
 
 for iter in range(num_samples):
     test_data[iter, np.random.randint(num_messages)] = 1
 
 np.save(file, test_data)
 print(test_data)
+
+test_data = np.zeros([num_samples, num_messages])
+
+for iter in range(num_samples):
+    test_data[iter, np.random.randint(num_messages)] = 1
+
+np.save(file1, test_data)
+
 file.close()
+file1.close()
